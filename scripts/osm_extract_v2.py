5959eedb2446889f2b050272f0fd14a151b89a2b
Silvia Colombo <silvia.colombo@ouce.ox.ac.uk>
2026-03-02 18:25:20 +0000

    extraction code for ports and airports, osm

---
+++ scripts/osm_extract_v2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+import os
+import re
+import json
+import pandas as pd
+import igraph as ig
+import geopandas as gpd
+from tqdm import tqdm
+tqdm.pandas()
+from pyrosm import OSM
+import quackosm as qo
+
+def load_config():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    config_path = os.path.join(script_dir,'..', 'config.json')
+
+    with open(config_path, 'r') as config_fh:
+        config = json.load(config_fh)
+    return config
+
+def main(config):
+    incoming_data_path = config['paths']['incoming_data']
+    processed_data_path = config['paths']['data']
+    in_path = os.path.join(incoming_data_path,"osm", "africa-260219.osm.pbf")
+    out_path = os.path.join(incoming_data_path,"infrastructure","africa_ports.parquet")
+
+    qo.convert_pbf_to_parquet(
+        # pbf_path='../datasets/OSM/raw/africa-latest.osm.pbf',
+        pbf_path=in_path,
+        result_file_path=out_path,
+        tags_filter={
+                "landuse": ["port"],
+            },
+            explode_tags=False,
+        )
+ 
+if __name__ == '__main__':
+    CONFIG = load_config()
+    main(CONFIG)
